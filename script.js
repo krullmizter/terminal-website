109dbf6115b11d00437acf551fba6d791d1aa7bd
krullmizter <samuelgranvik@protonmail.com>
2021-02-18 21:45:54 +0200

    Initial commit - Added auto-typing script. - Messed around with borders for the computer monitor.

---
+++ script.js
@@ -0,0 +1,14 @@
+var i = 0;
+var speed = 25;
+var text = 'Welcome to me, I\'m Samuel Granvik, a student of BE in information technology at Arcada University of Applied Sciences';
+
+window.onload = function autoType() {
+
+    if (i < text.length) {
+        document.getElementById('inner-terminal').innerHTML += text.charAt(i);
+        i++;
+        setTimeout(autoType, speed);
+    }
+    
+}
+

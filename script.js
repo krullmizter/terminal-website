284f79aac3f9582f36fb973bffbe0bdc5d364445
krullmizter <samuelgranvik@protonmail.com>
2021-02-19 21:46:01 +0200

    Added blinking cursor - Added turn-on effect. - Cleaned up code.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var i = 0;
 var speed = 25;
-var text = 'Welcome to me, I\'m Samuel Granvik, a student of BE in information technology at Arcada University of Applied Sciences';
+var text = 'Welcome to me, I\'m Samuel Granvik. I\'m a bachelor student of in information technology at Arcada University of Applied Sciences.';
 
 window.onload = function autoType() {
 
     if (i < text.length) {
-        document.getElementById('terminal').innerHTML += text.charAt(i);
+        document.getElementById('input').innerHTML += text.charAt(i);
         i++;
         setTimeout(autoType, speed);
     }
